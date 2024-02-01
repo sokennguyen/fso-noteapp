9ef6710c1de952d2088af969e86ffa298f079aee
soken <nsongkien@gmail.com>
2024-02-01 15:12:14 +0200

    edit: footer

--- part2-notes-frontend/src/components/Footer.jsx
+++ part2-notes-frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div style={footerStyle}>
       <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+      <em>Kien Nguyen, 2024</em>
     </div>
   )
 }
