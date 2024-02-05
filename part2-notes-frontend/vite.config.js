0a2f282912e89b29b5f3be2e6228c355a7f47453
soken <nsongkien@gmail.com>
2024-02-05 07:57:46 +0200

    edit: proxy origin

--- part2-notes-frontend/vite.config.js
+++ part2-notes-frontend/vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://noteapp-nskien.fly.dev',
         changeOrigin: true,
+        secure:false,
       },
     }
   },
